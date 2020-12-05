6c1d49ff9ae82aeacce48d2a6e5938d3ce8e564a
TanyaTaylor17 <72988872+TanyaTaylor17@users.noreply.github.com>
2020-12-05 19:44:11 +0000

    Update weather icon

--- src/index.js
+++ src/index.js
@@ -78,6 +78,15 @@ function displayWeather(response) {
   document.querySelector(
     "#description"
   ).innerHTML = `${response.data.weather[0].main}`;
+  document
+    .querySelector("#weather-icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  document
+    .querySelector("#weather-icon")
+    .setAttribute("alt", `${response.data.weather[0].description}`);
   document.querySelector("#humidity").innerHTML = `${Math.round(
     response.data.main.humidity
   )}`;
